4f6b58c830ee5ac91ad687e6be184f6a44442f8c
trossman <zacknafien2000@yahoo.com>
2016-07-12 23:07:54 -0500

    commit

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -10,7 +10,15 @@ namespace Homework4
     {
         static void Main(string[] args)
         {
+            Console.Write("Please enter a factorial to display: ");
+            string input = Console.ReadLine(); 
+            int x = int.Parse(input);
+            int[] factorials = new int[x];
 
+            for (int i = 1; i <= factorials.Length; i++)
+            {
+                
+            }
         }
     }
 }
